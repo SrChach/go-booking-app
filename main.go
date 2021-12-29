fc882f1b1b8bb1321e1d7211288b0d69b811fe7e
Ignacio Martinez Avila <ignacio.martinez@mediaagility.com>
2021-12-29 16:49:01 -0600

    FEATURE: Added types

--- main.go
+++ main.go
@@ -3,5 +3,25 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("hello world")
+	/** Constants */
+	const totalTickets = 50
+
+	/** Variables */
+	var conferenceName string = "My Conference"
+
+	/** Self typed variables */
+	remainingTickets := 50
+
+	/** You can also print the Type of a variable
+	  fmt.Printf(
+	    "The type of conferenceName is %T, and remainingTickets is %T\n",
+	    conferenceName, remainingTickets,
+	  )
+	*/
+
+	fmt.Printf("Welcome to the %s booking application\n", conferenceName)
+	fmt.Printf(
+		"You can book tickets here. %d of %v tickets remaining\n",
+		remainingTickets, totalTickets,
+	)
 }

c60ad2302c378cc55398ac94bc4200726670191a
Ignacio Martinez Avila <ignacio.martinez@mediaagility.com>
2021-12-31 11:34:52 -0600

    FEATURE: Added concepts of threading

--- main.go
+++ main.go
@@ -3,7 +3,11 @@ package main
 import (
 	"fmt"
 	"go-booking-app/booking"
+	"sync"
+	"time"
 )
+
+var waitGroup = sync.WaitGroup{}
 
 func main() {
 
@@ -20,9 +24,28 @@ func main() {
 			bookingInfo.Name, bookingInfo.LastName, bookingInfo.Quantity,
 		)
 
+		/** Add 1 thread to wait before the main thread closes */
+		waitGroup.Add(1)
+
+		/** The "go" keyword make a new thread */
+		go SendMailTicket()
+
+		/** Waits until all the open Threads finishes */
+		waitGroup.Wait()
+
 		bookings = append(bookings, bookingInfo)
 		remainingTickets -= bookingInfo.Quantity
 		fmt.Printf("Total bookings %v\n", bookings)
 	}
 
+}
+
+func SendMailTicket() {
+	time.Sleep(1 * time.Second)
+	fmt.Println("###############")
+	fmt.Println("Your ticket are being sent")
+	fmt.Println("###############")
+
+	/** Closes the thread */
+	waitGroup.Done()
 }
